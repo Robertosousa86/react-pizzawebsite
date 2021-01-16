d4b1fb46c834128a3f006ef89f5e22427cb19e02
Roberto Sousa <robertosousa186@gmail.com>
2021-01-16 02:07:39 -0200

    creating the hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "../Navbar";
-import { HeroContainer } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
 const Hero = () => {
   return (
     <HeroContainer>
       <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Kauabanga! it's Pizza Time!</HeroH1>
+          <HeroP>Like a Pizza ninja dude!</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   );
 };
