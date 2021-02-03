962056b205cd6ffac5ba48bab134db980774ce72
Roberto Sousa <robertosousa186@gmail.com>
2021-02-03 01:07:34 -0200

    ending the features section

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
-import { productData } from "./components/Products/data";
-import Feature from './components/Feature'
+import { productData, productDataTwo } from "./components/Products/data";
+import Feature from "./components/Feature";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favorite favorite" data={productData} />
       <Feature />
+      <Products heading="Sweet treats for You!" data={productDataTwo} />
     </Router>
   );
 }
